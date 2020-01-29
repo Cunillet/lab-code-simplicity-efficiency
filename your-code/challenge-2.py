7f85eeab3035b1347fe701f52846de45179f1dd2
Oriol Cunill <cunillati@gmail.com>
2020-01-29 11:15:02 +0100

    Challenge 2 fixed

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -1,38 +1,38 @@
-"""
-The code below generates a given number of random strings that consists of numbers and 
-lower case English letters. You can also define the range of the variable lengths of
-the strings being generated.
+import random
+from string import ascii_letters as letters
 
-The code is functional but has a lot of room for improvement. Use what you have learned
-about simple and efficient code, refactor the code.
-"""
+class StringGenerator:
+    def __init__(self):
+        self.minimum = 0
+        self.maximum = 0
+        self.count = 0
+    
+    def get_rand_letter(self):
+        return random.choice(str.lower(letters))
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+    def get_rand_digit(self):
+        return random.randint(0, 9)
+    
+    def get_rand_str(self):
+        if bool(random.getrandbits(1)):
+            return self.get_rand_letter()
+        return str(self.get_rand_digit())
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+    def generate_rand_str(self, l):
+        return ''.join([self.get_rand_str() for i in range(l)])
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+    def request_basics(self):
+        self.minimum = int(input('Enter minimum string length: '))
+        self.maximum = int(input('Enter maximum string length: '))
+        self.count = int(input('How many random strings to generate? '))
+    
+    def get_requested_str(self):
+        for _ in range(self.count):
+            l = random.randint(self.minimum, self.maximum)
+            print(self.generate_rand_str(l))
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+
+if __name__ == '__main__':
+    sg = StringGenerator()
+    sg.request_basics()
+    sg.get_requested_str()
